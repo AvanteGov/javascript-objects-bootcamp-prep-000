91833a7303d2b15c324155163a96e4abc3843a35
grateful-library-0184 <alexcouts18@gmail.com>
2019-03-01 23:13:22 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var meals = {};
+
+var meals = new Object();
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value
+ 
+  return obj
+}
+ 
+const recipe = { eggs: 3 }
